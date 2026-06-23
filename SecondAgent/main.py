bd1da40ef58412b12672e3e64b5679aa512f5dcd
Vincenzo Sorano <vincenzo.sorano@bectran.com>
2026-06-23 10:44:00 -0500

    added a third node
    The third node I custom made to make sure I understand edges and nodes in a sequential graph.

--- SecondAgent/main.py
+++ SecondAgent/main.py
@@ -5,16 +5,28 @@ from langgraph.graph import StateGraph
 class AgentState(TypedDict):
     name: str
     age: str
+    skills: List[str] 
     final: str
 
 def first_node(state: AgentState):
-    """This is the first node of our sequence"""
-    state['final'] = f"Hi {state['name']} "
+    """Personalize the name fiel with a greeeting"""
+    state['final'] = f"Hi {state['name']}, welcome to langraph! "
     return state
 
 def second_node(state: AgentState):
     """This is the second node of our sequence"""
-    state['final'] = state['final'] + f"your age is {state['age']}"
+    state['final'] = state['final'] + f"your are {state['age']} and have some impressive skills!"
+    return state
+
+def third_node(state: AgentState):
+    """This is the third node of our sequence, it appends the users skills to the string"""
+    skills = state['skills']
+    if len(skills) == 1:
+        skills_str = skills[0]
+    else:
+        skills_str = ", ".join(skills[:-1]) + f", and {skills[-1]}"
+
+    state['final'] = state['final'] + f" You have skills in: {skills_str}"
     return state
 
 if __name__ == "__main__":
@@ -22,13 +34,17 @@ if __name__ == "__main__":
 
     graph.add_node("first_node", first_node)
     graph.add_node("second_node", second_node)
+    graph.add_node("third_node", third_node)
 
     graph.set_entry_point("first_node")
+
     graph.add_edge("first_node", "second_node")
-    graph.set_finish_point("second_node")
+    graph.add_edge("second_node", "third_node")
+
+    graph.set_finish_point("third_node")
 
     app = graph.compile()
-    result = app.invoke({"name": "enzo", "age": 26})
+    result = app.invoke({"name": "enzo", "age": 26, "skills": ["c++", "python", "cooking"]})
 
     print(result)
 
